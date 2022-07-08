13e1890467cff73a05c9f4ce653acb987b71283b
faishalfirmanh <isalredarmy1@gmail.com>
2022-07-08 18:32:50 +0700

    1

--- logika.php
+++ logika.php
@@ -195,4 +195,77 @@ function findDigits($tot,$bil){
 }
 // findDigits($tot_input_bil,$decimal);
 
+//----------------------------- the-hurdle-race
+  //$input_chacacter = trim(fgets(STDIN));
+  //$input_obstacle = trim(fgets(STDIN));
+
+  function hitungBerhasil($prof,$rintangan){
+    $to_ar_prof = explode(" ",$prof);
+    if (count($to_ar_prof) != 2) {
+       echo "Error First input most 2";
+       die();
+    }
+    $to_ar_rin = explode(" ",$rintangan);
+    $length_of_obstacle = $to_ar_prof[0];
+    $max_jump = $to_ar_prof[1];
+
+    $yang_dapat_dilompati = array();//kalau 0 berhasil semua
+    $yang_tidak_dapat = array();
+    $yang_tidak_dapat_100 = array();
+    if ($length_of_obstacle != count($to_ar_rin)) {
+       echo "Error ! obstacle must be same";
+       die();
+    }
+     foreach ($to_ar_rin as $key) {
+        if (intval($key) > intval($max_jump)) {
+            if (count($to_ar_rin) > 99) {
+               $res = count($to_ar_rin)-intval($max_jump);
+               array_push($yang_tidak_dapat_100,$res);
+            }else {
+               array_push($yang_tidak_dapat,$key);
+            }
+        }
+     }
+     $final_res = count($yang_tidak_dapat_100) > 0 ? $yang_tidak_dapat_100[0] : count($yang_tidak_dapat);
+     echo $final_res;
+  }
+//hitungBerhasil($input_chacacter,$input_obstacle);
+//---------------------------------------------------
+//https://www.hackerrank.com/challenges/quicksort1/problem?isFullScreen=true
+   // $input_tot_arr = trim(fgets(STDIN));
+   // $input_list_ar =  trim(fgets(STDIN));
+
+   function urutkan($a,$b){
+     $ar = explode(" ",$b);
+     if (count($ar) != $a) {
+        echo "Not same";
+        die();
+     }
+     sort($ar);
+     $arr_empty = array();
+     for($x=0;$x<count($ar);$x++)
+     {
+        $urut =  $ar[$x];
+        array_push($arr_empty,$urut);
+     }
+     $to_str = implode(" ",$arr_empty);
+     echo $to_str;
+   }
+   //urutkan($input_tot_arr,$input_list_ar);
+//----------------------------------------------
+//https://www.hackerrank.com/challenges/migratory-birds/problem?isFullScreen=true
+// $input_tot_arr = trim(fgets(STDIN));
+// $input_list_ar =  trim(fgets(STDIN));
+function migrationGroup($totalItem,$list){
+  $arr = explode(" ",$list);
+  if (count($arr) != $totalItem) {
+      echo "ERROR NOT SAME";
+      die();
+  }
+   $cc = array_count_values($arr); //menjadi item array jumlahnya berapa
+   $max = max($cc);
+   $new_arr = min(array_keys($cc, max($cc))); //membalik ->cari yang terkecil result mesti 1
+   echo $new_arr;
+}
+// migrationGroup($input_tot_arr,$input_list_ar);
  ?>
