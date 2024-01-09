3d7a24d6904a775d3e063d19f56fbeb08b9e7f15
isal48 <isalredarmy1@gmail.com>
2024-01-09 15:11:31 +0700

    palindrome

--- 3log.php
+++ 3log.php
@@ -29,4 +29,42 @@ function MinumumDeleted($input,$listAr){
 
 
 
+
+function palindrome($s){ //polindrome ->sukses
+
+   if (strpos($s, ' ') !== false) { //kalimat yang mengandung spasi
+     $cccc =  preg_replace('/\s+/', '', strtolower($s));
+     $toAr =  str_split($cccc,1);
+  } else { //kata
+     $toAr = str_split($s,1);
+  }
+  
+   $length = count($toAr);
+
+   $b = $length-1;
+   $totSame = 0;
+   for ($i=0; $i < count($toAr) ; $i++) { 
+      $kiri =  preg_replace('/\s+/', '', strtolower($toAr[$i]));
+      $kanan = preg_replace('/\s+/', '', strtolower($toAr[$b]));
+      if ($kiri == $kanan ) {
+         $totSame++;
+      }
+      $b--;
+   }
+   
+   $cekResult = $length == $totSame ? 'poliandrome' : 'not';
+ 
+   echo $cekResult;
+ 
+}
+// $inputstr = "Was it a rat i sawXZ";
+// palindrome($inputstr);
+
+
+//https://www.hackerrank.com/challenges/the-love-letter-mystery/problem?isFullScreen=true -> not solved
+//love letter mistery
+//hanya dapat mengubah 1 x tiap perubagab
+//2, hanya dapat mengubah dari d->c dan c tidak bisa diubah ke d
+//
+
 ?>
